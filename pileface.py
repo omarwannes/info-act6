ef427640259ce778134389177d35d14c0d1eea24
omarwannes <wannesazerty11@gmail.com>
2022-11-09 13:51:31 -0800

    first commit

---
+++ pileface.py
@@ -0,0 +1,15 @@
+from random import*
+print('player 1')
+p1=input('****tape your name****')
+x = 1
+print(p1,'is number',x)
+print('player 2')
+p2=input('****tape your name****')
+y = 0
+print(p2,'is number',y)
+r=randint(0,1)
+print(r)
+if r== 1:
+    print('player 1 win')
+else :
+    print('player 2 win')
